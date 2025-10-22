ac7dd91f25541d69b0bcffc2210f5e74457d8532
mkrooted256 <mykhailo.koreshkov@gmail.com>
2025-10-22 21:22:19 +0300

    adjust reauth flow

--- tools.py
+++ tools.py
@@ -40,6 +40,7 @@ from datetime import datetime, timezone
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
+from google.auth.exceptions import RefreshError
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
@@ -159,8 +160,12 @@ def auth_drive(token_file):
         # If there are no (valid) credentials available, let the user log in.
         if not creds or not creds.valid:
             if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
+                try:
+                    creds.refresh(Request())
+                except RefreshError:
+                    logger.info("Stored credentials expired and refresh failed. Re-authenticating.")
+                    creds = None
+            if not creds:
                 flow = InstalledAppFlow.from_client_secrets_file(
                     "credentials.json", SCOPES
                 )
